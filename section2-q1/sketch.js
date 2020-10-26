dfca1bf63a0bb7ee66b47cf46df7681fedf58922
Shusuke8 <72759539+Shusuke8@users.noreply.github.com>
2020-10-26 22:48:24 +0900

    section2 questions

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let x = i * 10;
+    if(i >= 4){
+      noFill();
+      ellipse(50, 50, x);
+      stroke(255, 0, 0);
+    }
+    else{
+      noFill();
+      ellipse(50, 50, x);
+      stroke(0, 0, 255);
+    }// BLANK[1]
   }
+
 }
