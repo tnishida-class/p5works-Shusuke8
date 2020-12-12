ff11041547c6d994e217c6573955d284e6116ba8
Shusuke8 <72759539+Shusuke8@users.noreply.github.com>
2020-12-13 03:44:19 +0900

    section3-q1 修正版

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,16 +4,16 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 120, 178, 100, 162);
 }
 
-function balloon(t, x, y){
+function balloon(t, a, b, c){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   noStroke();
-  x = fill(178, 100, 162);
-  y = rect(0, 0, w + p * 2, h + p * 2);
+  fill(a, b, c);
+  rect(0, 0, w + p * 2, h + p * 2);
   triangle(w + p * 2, h + p * 2, w + p * 2 - 10, h + p * 2, w + p * 2, h + p * 2 + 10);
   fill(255);
   text(t, p, h + p);
