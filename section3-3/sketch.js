bd278723a1a540167bb1958c53321fd5fee3089f
Shusuke8 <72759539+Shusuke8@users.noreply.github.com>
2020-12-14 03:38:40 +0900

    section3-3

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -15,8 +15,31 @@ function setup(){
 
 function calendar(y, m){
   let dow = dayOfWeek(y, m, 1);
+  let s = 0;
+  let t = 20;
+
+  textSize(20);
+  text(y + "/" + m, 68, 20);
+
+  textSize(13);
+  for(i = 0; i < 7; i++){
+    let x;
+    x = dayOfWeekAsString(i);
+    text(x, 25 * (i + 1), 41);
+  }
+
   for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+   dow = dayOfWeek(y, m, d);
+   textSize(15);
+   if(d < 10){
+     text(d, (dow * 25 + t + 5), (s + 68));
+   }
+   else{
+     text(d, (dow * 25 + t), (s + 68));
+   }
+   if(dow == 6){
+    s += 25;
+   }
   }
 }
 
@@ -26,6 +49,7 @@ function isLeapYear(y){
 
 function daysInYear(y){
   // BLANK[1]
+  return isLeapYear(y) ? 366 : 365;
 }
 
 function daysInMonth(y, m){
@@ -50,6 +74,14 @@ function dayOfYear(y, m, d){
 
 function dayOfWeek(y, m, d){
   // BLANK[2]
+  let a = dayOfYear(y, m, d) % 7;
+  console.log(a);
+  if(a == 6){
+    return 0;
+  }
+  else{
+    return a + 1;
+  }
 }
 
 function dayOfWeekAsString(dow){
